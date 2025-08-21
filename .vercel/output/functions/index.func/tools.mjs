1fcce6eac8eeaf3ef881950f18eb529d4f94fd99
Taofeeq Oluder <oluderutaofeeq@gmail.com>
2025-08-21 11:24:07 +0100

    use latest

---
+++ .vercel/output/functions/index.func/tools.mjs
@@ -0,0 +1,3 @@
+import * as tool0 from './tools/a1bff328-1a2b-47d0-a3d0-af7c81196e95.mjs';
+
+export const tools = [tool0]
---
+++ .vercel/output/functions/index.func/tools/a1bff328-1a2b-47d0-a3d0-af7c81196e95.mjs
@@ -0,0 +1,6 @@
+export { fw as weatherTool } from '../mastra.mjs';
+import 'pino';
+import 'pino-pretty';
+import 'stream/web';
+import 'crypto';
+import 'events';
