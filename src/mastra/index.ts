951d0293b1db9256afddb4d4071843781a0e13c3
Taofeeq Oluder <oluderutaofeeq@gmail.com>
2025-08-21 11:22:31 +0100

    set up vercel

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,13 +1,15 @@
-import { Mastra } from '@mastra/core/mastra';
-import { PinoLogger } from '@mastra/loggers';
-import { weatherWorkflow } from './workflows';
-import { weatherAgent } from './agents';
+import { Mastra } from "@mastra/core/mastra";
+import { PinoLogger } from "@mastra/loggers";
+import { weatherWorkflow } from "./workflows";
+import { weatherAgent } from "./agents";
+import { VercelDeployer } from "@mastra/deployer-vercel";
 
 export const mastra = new Mastra({
   workflows: { weatherWorkflow },
   agents: { weatherAgent },
   logger: new PinoLogger({
-    name: 'Mastra',
-    level: 'info',
+    name: "Mastra",
+    level: "info"
   }),
+  deployer: new VercelDeployer()
 });
